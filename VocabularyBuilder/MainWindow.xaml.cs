76c9558b8d06628605834603e5050e16f4f49d0b
Sherzod <sherzodyuldashef@gmail.com>
2023-06-17 23:31:18 +0500

    Designed main dashboard

--- VocabularyBuilder/MainWindow.xaml.cs
+++ VocabularyBuilder/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using VocabularyBuilder.Pages;
 namespace VocabularyBuilder
 {
     /// <summary>
@@ -23,6 +23,57 @@ namespace VocabularyBuilder
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void brDragable_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void rbTopic1_Click(object sender, RoutedEventArgs e)
+        {
+            TopicPage1 topicPage1 = new TopicPage1();
+            PageNavigator.Content = topicPage1;
+        }
+
+        private void rbTopic2_Click(object sender, RoutedEventArgs e)
+        {
+            TopicPage2 topicPage2 = new TopicPage2();
+            PageNavigator.Content = topicPage2;
+        }
+
+        private void rbTopic3_Click(object sender, RoutedEventArgs e)
+        {
+            TopicPage3 topicPage3 = new TopicPage3();
+            PageNavigator.Content = topicPage3;
+        }
+
+        private void rbTopic4_Click(object sender, RoutedEventArgs e)
+        {
+            TopicPage4 topicPage4 = new TopicPage4();
+            PageNavigator.Content = topicPage4;
         }
     }
 }
---
+++ VocabularyBuilder/Pages/TopicPage1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VocabularyBuilder.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TopicPage1.xaml
+    /// </summary>
+    public partial class TopicPage1 : Page
+    {
+        public TopicPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
